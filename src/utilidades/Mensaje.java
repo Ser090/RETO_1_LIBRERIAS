196d4c7b50891e31bc8b2e73245f81d552151602
Sergio <programacion.software.00@gmail.com>
2024-10-04 12:13:03 +0200

    Estructura Basica

---
+++ src/utilidades/Mensaje.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidades;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Mensaje {
+
+    private Enum mensaje;
+    private User usuario;
+
+}
